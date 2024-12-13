5af8fe32a4012bdb46c05e55bcf4daf74de4a39f
oden6680 <oden6680@gmail.com>
2024-12-13 16:12:33 +0900

    ルールの改定

--- src/components/HelpDialog.tsx
+++ src/components/HelpDialog.tsx
@@ -12,26 +12,36 @@ interface HelpDialogProps {
   onClose: () => void;
 }
 
- const HelpDialog = ({ open, onClose }: HelpDialogProps) => {
+const HelpDialog = ({ open, onClose }: HelpDialogProps) => {
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>ゲームのルール</DialogTitle>
+      <DialogTitle>ポケミアリーグSeason1レギュレーション</DialogTitle>
       <DialogContent>
         <Typography gutterBottom>
-          1. 毎週火曜日から開始される総当たりリーグ戦です。
+          1. 拡張パック「幻のいる島」発売日(12/17)から開始されるリーグ戦です。
         </Typography>
         <Typography gutterBottom>
           2.
-          各試合では2～5ゲームが行われ、先に3ゲーム勝利したプレイヤーがその試合の勝者となります。
+          各週4名が総当たりで試合を行い、各試合では先に3ゲーム勝利したプレイヤーがその週の該当試合の勝者となります。
         </Typography>
         <Typography gutterBottom>
-          3. 勝利した試合数が多いプレイヤーが上位にランクされます。
+          3.
+          試合の勝利数に応じて、勝者には3点、敗者には0点の勝ち点が与えられます。
         </Typography>
         <Typography gutterBottom>
-          4. 各ゲームのスコアと使用デッキのタイプが記録されます。
+          4.
+          12週間のリーグ戦を終えた時点で、合計勝ち点の多い順に順位が決定されます。
         </Typography>
         <Typography gutterBottom>
-          5. 勝利数が同じ場合は、合計スコア差で順位を決定します。
+          5.
+          勝利数が同じ場合は、合計スコア(各ゲームでの獲得ポイント数の総和)差で順位を決定します。
+        </Typography>
+        <Typography gutterBottom>
+          6. スコア差も同じ場合は、直接対決の結果で順位を決定します。
+        </Typography>
+        <Typography gutterBottom>
+          7.
+          各試合でプレイヤーは3種類のデッキを用意し、1試合目の開始前にそれぞれのデッキで使用するエネルギーを相手に伝える必要があります。この時、どの対戦でどのデッキを使用するかを伝える必要はありません。
         </Typography>
       </DialogContent>
       <DialogActions>
